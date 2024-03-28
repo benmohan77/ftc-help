953b01fc92e66a97c5630a5552da74e72bcd3ce9
Benjamin Mohan <benmohan77@gmail.com>
2024-03-27 20:41:27 -0500

    Added fake tests

---
+++ TeamCode/src/test/java/TestingTests.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestingTests {
+
+    @Test
+    public void test() {
+        Boolean test = false;
+        assertEquals(test, false);
+    }
+}
